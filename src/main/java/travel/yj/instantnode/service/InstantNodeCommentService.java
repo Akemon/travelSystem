c6a7c28169cb40b9ba32bc6b96d5437b1c80debf
826299017 <826299017@qq.com>
2018-01-23 20:49:32 +0800

    modified by yj 2018年1月23日20:31:46

--- src/main/java/travel/yj/instantnode/service/InstantNodeCommentService.java
+++ src/main/java/travel/yj/instantnode/service/InstantNodeCommentService.java
@@ -1,8 +1,6 @@
 package travel.yj.instantnode.service;
 
-import org.springframework.stereotype.Service;
 
-@Service
 public class InstantNodeCommentService {
 
 }
--- src/main/java/travel/yj/instantnode/service/InstantNodeService.java
+++ src/main/java/travel/yj/instantnode/service/InstantNodeService.java
@@ -1,7 +1,5 @@
 package travel.yj.instantnode.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public class InstantNodeService {
+
 }
