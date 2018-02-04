3494faba289ef3810e2c8ab9f133d1531e521604
826299017 <826299017@qq.com>
2018-02-04 17:27:18 +0800

    modified by yj 2018年2月4日17:26:20

---
+++ src/main/java/travel/yj/followlist/test/FollowPeopleListServiceTest.java
@@ -0,0 +1,45 @@
+package travel.yj.followlist.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import travel.yj.followlist.service.FollowPeopleListService;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author 杨景
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class FollowPeopleListServiceTest {
+
+    @Autowired
+    private FollowPeopleListService followPeopleListService;
+
+    @Test
+    public void testFolowSomeone(){
+        String result=followPeopleListService.folowSomeone("dsf","826299017");
+        assertEquals(true,result.indexOf("成功")>=0);
+    }
+
+    @Test
+    public void testCancelFollowSomeOne(){
+        String result=followPeopleListService.cancelFollowSomeOne(1);
+        assertEquals(true,result.indexOf("成功")>=0);
+    }
+
+    @Test
+    public void testSelectMyFans(){
+        String result=followPeopleListService.selectMyFans("826299017");
+        System.out.println(result);
+    }
+
+    @Test
+    public void testSelectMyFollowUser(){
+        String result=followPeopleListService.selectMyFollowUser("826299017");
+        System.out.println(result);
+    }
+}
