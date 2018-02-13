df60d9af70b93eb129ede3d08f49effec64e7939
826299017 <826299017@qq.com>
2018-02-13 18:23:21 +0800

    modified by yj 2018年2月12日21:25:26

---
+++ src/main/java/travel/yj/instantnode/controller/InstantNoteLikePeopleController.java
@@ -0,0 +1,34 @@
+package travel.yj.instantnode.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import travel.yj.instantnode.service.InstantNoteLikePeopleService;
+
+/**
+ * @author 杨景
+ */
+@Controller
+@RequestMapping("InstantNoteLikePeople")
+public class InstantNoteLikePeopleController {
+
+    @Autowired
+    private InstantNoteLikePeopleService instantNoteLikePeopleService;
+
+    @RequestMapping(value="addInstantNoteLike",method = RequestMethod.POST,produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String addInstantNoteLike(Integer instantNoteId,String creatorId){
+
+        return instantNoteLikePeopleService.addInstantNoteLikePeople(instantNoteId,creatorId);
+    }
+
+    @RequestMapping(value="cancelLikeInstantNote",method = RequestMethod.POST,produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String  cancelLikeInstantNote(Integer instantNoteId,String creatorId){
+        return instantNoteLikePeopleService.cancelLikeInstantNote(instantNoteId,creatorId);
+    }
+
+
+}
